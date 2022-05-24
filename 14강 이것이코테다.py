80219f03394a0e4550da5aede23c5dc333856f9f
정민킴 <jma1020@naver.com>
2022-05-24 23:58:47 +0900

    22-05-24 코테 문제 풀이

---
+++ 14강 이것이코테다.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+a, b == 1,1 # 초기 좌표
+plans = input().split()
+
+x = [0, -1, 0 ,1]
+y = [1, 0, -1, 0]
+  # 동 북 서 남
+  # 우 상 좌 하
+move_types = ['R' , 'U', 'L', 'D']
+
+# 이동 계획을 하나씩 확인
+for plan in plans:
+  for i in range(len(move_types)):
+    if plan == move_types[i]:
+      nx = a + x[i]
+      ny = b + y[i]
+
+  if nx < 1 or ny <1 or nx > n or ny > n:
+    continue
+    
+
+ 
